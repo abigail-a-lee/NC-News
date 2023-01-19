72275d305156175b4d906a0ae99bf6221754ec55
abigail-a-lee <aabigaillee@gmail.com>
2023-01-19 16:10:02 +0000

    + POST /api/articles/:article_id/comments endpoint

--- controllers/articles.js
+++ controllers/articles.js
@@ -8,14 +8,31 @@ exports.getArticles = (req, res, next) => {
     .then((articles) => {
       res.status(200).send({ articles });
     })
-    .catch(next);
+    .catch((err) => {
+      err.status = 500;
+      return next(err);
+    });
 };
 
 exports.getArticleById = (req, res, next) => {
   const id = req.params.article_id;
+  if (isNaN(parseInt(id))) {
+    const err = new Error("Bad Request: ID must be a number");
+    err.status = 400;
+    return next(err);
+  }
+
   selectArticleById(id)
     .then((article) => {
+      if (article.length === 0) {
+        const err = new Error("Article not found");
+        err.status = 404;
+        return next(err);
+      }
       res.status(200).send({ article });
     })
-    .catch(next);
+    .catch((err) => {
+      err.status = 500;
+      return next(err);
+    });
 };
--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,4 @@ exports.getTopics = require("./topics");
 exports.getArticles = require("./articles");
 exports.getArticleById = require("./articles");
 exports.getCommentsById = require("./comments");
+exports.postComment = require("./comments");
