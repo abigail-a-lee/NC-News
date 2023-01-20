82a186d2e29ac1f6c6307a471a87e8a56deb92d0
abigail-a-lee <aabigaillee@gmail.com>
2023-01-20 12:39:34 +0000

    adds DELETE /api/comments/:comment_id endpoint

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,11 +2,8 @@ const {
   selectCommentsById: { selectCommentsById },
   insertComment: { insertComment },
   selectArticleById: { selectArticleById },
+  removeCommentById: { removeCommentById },
 } = require("../models");
-
-const {
-  getArticleById: { getArticleById },
-} = require("../controllers");
 
 exports.getCommentsById = (req, res, next) => {
   const id = req.params.article_id;
@@ -76,6 +73,31 @@ exports.postComment = (req, res, next) => {
             return next(err);
           });
       }
+    })
+    .catch((err) => {
+      err.status = 500;
+      return next(err);
+    });
+};
+
+exports.deleteCommentById = (req, res, next) => {
+  const id = req.params.comment_id;
+
+  // Input validation
+  if (isNaN(parseInt(id))) {
+    const err = new Error("Bad Request: ID must be a number");
+    err.status = 400;
+    return next(err);
+  }
+
+  removeCommentById(id)
+    .then((comment) => {
+      if (comment.length === 0) {
+        const err = new Error("Cannot delete comment that does not exist");
+        err.status = 404;
+        return next(err);
+      }
+      res.status(204).send();
     })
     .catch((err) => {
       err.status = 500;
